85a9995298c3891d11f5e4b22ab4604d9b82c3c7
Eric Gallager <egallager@comcast.net>
2013-06-14 20:17:20 -0400

    tweak

--- autogen.sh
+++ autogen.sh
@@ -1,12 +1,13 @@
-#! /bin/sh
+#!/bin/sh
 
 srcdir=`dirname $0`
 test -z "$srcdir" && srcdir=.
 
+# Is setting ORIGDIR here necessary when the $OLDPWD variable is available?
 ORIGDIR=`pwd`
 cd $srcdir
 
-autoreconf -v --install || exit 1
+autoreconf -f -v --install || exit 1
 cd $ORIGDIR || exit $?
 
 $srcdir/configure --enable-maintainer-mode "$@"
