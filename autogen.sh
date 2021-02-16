7c0603d09bc3e18c657c8d40fbd6c318f268dd4a
Eric Gallager <egall@gwmail.gwu.edu>
2021-02-16 11:39:44 -0500

    minor autotools update

--- autogen.sh
+++ autogen.sh
@@ -4,14 +4,14 @@ echo "This script basically just runs \"autoreconf\"."
 echo "Feel free to just run \"autoreconf\" yourself with your favorite flags instead."
 
 srcdir=`dirname $0`
-test -z "$srcdir" && srcdir=.
+test -z "${srcdir}" && srcdir=.
 
 # Is setting ORIGDIR here necessary when the $OLDPWD variable is available?
 ORIGDIR=`pwd`
-cd $srcdir
+cd ${srcdir}
 
 autoreconf -f -v --install || exit 1
-cd $ORIGDIR || exit $?
+cd ${ORIGDIR} || exit $?
 
-$srcdir/configure --enable-maintainer-mode "$@"
+test -x ${srcdir}/configure && ${srcdir}/configure --enable-maintainer-mode "$@"
 
